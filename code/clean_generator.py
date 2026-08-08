f4c5e9a7baf7ee41de4ba96cc4320cce374b2f00
Siddhartha Dikshit <sd.siddhartha.06@gmail.com>
2026-08-08 14:42:32 +0530

    feat(data): add QPSK modulation
    - Added QPSK modulation
    - Updated the data generator

--- code/clean_generator.py
+++ code/clean_generator.py
@@ -9,24 +9,56 @@ def generate_bits(bit_num):
 
 
 def bpsk_mod(bits):
-    """Converts raw signals into BPSK modulated symbols"""
+    """Converts raw signals into BPSK modulated symbols
+    0 -> -1
+    1 -> +1    
+    """
     bpsk_symbols = 2 * bits - 1
     return bpsk_symbols
 
 
 def qpsk_mod(bits):
-    print("Needs to be added.")
-    #qpsk_symbols = 
-    #return qpsk_symbols
+    """Converts raw signals into QPSK modulated symbols
+    It considers a set of 2 bits
+    sqrt(2) is used to normalize such that the average symbol power = 1
+    00 -> 1 + j / sqrt(2)
+    01 -> -1 + j/ sqrt(2)
+    11 -> -1 -j/ sqrt(2)
+    10 ->  1 - j/ sqrt(2)    
+    """
+
+    if len(bits)%2==0:
+        pairs = bits.reshape(-1,2)
+
+        qpsk_symbols = np.empty(len(pairs),dtype = np.complex64)
+
+        for i, (bit1,bit2) in enumerate(pairs):
+            # bit 1 -> complex part
+            if bit1 == 0:
+                imag = 1
+            else:
+                imag = -1
+
+            # bit 2 -> real part    
+            if bit2 == 0:
+                real = 1
+            else:
+                real = -1
+            qpsk_symbols[i] = (real + (imag*1j))/np.sqrt(2)
+        return qpsk_symbols
+    else :
+        return None
 
 def generate_data(samples = 10000, signal_length = 1024):
-    """Dataset Generation Test"""
+    """
+    Dataset Generation
 
-
+    Twice the number of samples are required for QPSK as compared to BPSK since each symbol in QPSK represents 2 bits
+    
+    """
     modulations = ["BPSK", "QPSK"]
     
-    # only generate bpsk for now, qpsk will be added later
-    for modulation in modulations[:1]:
+    for modulation in modulations:
 
         save_dir = f"data/{modulation.lower()}"
         os.makedirs(save_dir, exist_ok=True)
@@ -46,16 +78,18 @@ def generate_data(samples = 10000, signal_length = 1024):
                 clean_signals = np.empty((samples,signal_length), dtype=np.float32)
                 
             elif modulation == "QPSK":
-                clean_signals = np.empty((samples, signal_length // 2), dtype=np.complex64)
+                clean_signals = np.empty((samples, signal_length), dtype=np.complex64)
 
             for i in range(samples):
                 
-                bits = generate_bits(signal_length)
+                
             
                 if modulation == "BPSK":
+                    bits = generate_bits(signal_length)
                     symbols = bpsk_mod(bits).astype(np.float32)
                     
                 elif modulation == "QPSK":
+                    bits = generate_bits(signal_length * 2) 
                     symbols = qpsk_mod(bits).astype(np.complex64)
 
                 clean_signals[i] = symbols
